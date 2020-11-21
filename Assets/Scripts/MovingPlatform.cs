a5a1affbb5cdd75130dcbb1e7f78164844af8bbe
pavel <pavel.arapov@gmail.com>
2020-11-21 10:58:17 +0100

    feat(physics2d): home work with jump and dead area for game over detection, platform with multiple waypoints

--- Assets/Scripts/MovingPlatform.cs
+++ Assets/Scripts/MovingPlatform.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public class MovingPlatform : MonoBehaviour
 {
-    public Transform top;
-    public Transform bottom;
+    public Transform previousWaypoint;
+    public Transform nextWaypoint;
     public float speed;
+    public Transform[] waypoints;
+    public int waypointIndex = 0;
 
     private float current;  // от 0 до 1
     private float dir;
@@ -15,20 +17,29 @@ public class MovingPlatform : MonoBehaviour
     {
         current = 0.0f;
         dir = 1.0f;
+        previousWaypoint = waypoints[0];
+        nextWaypoint = waypoints[1];
+        waypointIndex = 1;
     }
 
     void Update()
     {
         current += dir * speed * Time.deltaTime;
+        
+        // find next waypoint if the current has been reached
         if (current > 1.0f) {
-            current = 1.0f;
-            dir = -1.0f;
-        } else if (current < 0.0f) {
             current = 0.0f;
-            dir = 1.0f;
-        }
+            previousWaypoint = waypoints[waypointIndex];
+            waypointIndex = FindNextWayPoint(waypointIndex);
+            nextWaypoint = waypoints[waypointIndex];
+        } 
+        
+        transform.position = Vector3.Lerp(previousWaypoint.position, nextWaypoint.position, current);
+    }
 
-        transform.position = Vector3.Lerp(top.position, bottom.position, current);
+    private int FindNextWayPoint(int waypoint) {
+        waypoint += 1;
+        return (waypoint % waypoints.Length) == 0 ? 0 : waypoint;
     }
 }
 
--- Assets/Scripts/TriggerDetector.cs
+++ Assets/Scripts/TriggerDetector.cs
@@ -5,9 +5,13 @@ using UnityEngine;
 public class TriggerDetector : MonoBehaviour
 {
     public bool inTrigger;
+    public bool isDeadArea = false;
 
     void OnTriggerEnter2D(Collider2D collision)
     {
+        if (collision.gameObject.CompareTag("DeadArea")) {
+            isDeadArea = true;
+        }
         inTrigger = true;
     }
 
